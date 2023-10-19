15e1ed9360f9656f2056ce1ffa9f7b6565421908
brendan-kearney <87655922+brendan-kearney@users.noreply.github.com>
2023-10-19 13:37:12 -0400

    Update consts.py

--- v1.0/json-generator/consts.py
+++ v1.0/json-generator/consts.py
@@ -25,13 +25,13 @@ class ReferenceDataset(object):
         if _genome == 'hg38' or _genome == 'grch38':
             self.bowtie_genome_ref_first_index = "/data/reddylab/Reference_Data/IGVF/GRCh38/bowtie_GRCh38/IGVFFI0653VCGH_GRCh38.1.ebwt"
             self.bowtie2_genome_ref_first_index = "/data/reddylab/Reference_Data/IGVF/GRCh38/bowtie2_GRCh38/IGVFFI0653VCGH_GRCh38.1.bt2" #TODO: Add ERCC92 Bowtie2 option
-            self.star_genome_dir = "/data/reddylab/Reference_Data/IGVF/GRCh38/STAR_GRCh38_genome/" % (self.read_length-1)
+            self.star_genome_dir = "/data/reddylab/Reference_Data/IGVF/GRCh38/STAR_genome_sjdbOverhang_%d_novelSJDB" % (self.read_length-1)
             self.encode_blacklist_bedfile = "/data/reddylab/Reference_Data/IGVF/GRCh38/hg38.blacklist.v3.bed"
             self.genome_effective_size = "hs"
-            self.annotation_file = "/data/reddylab/Reference_Data/IGVF/GRCh38/Gencode/v43/IGVFFI7217ZMJZ.gtf" % (".with_ercc92" if with_ercc else "")
-            self.rsem_dir = "/data/reddylab/Reference_Data/IGVF/GRCh38/RSEM_genome"% (".with_ercc92" if with_ercc else "")
+            self.annotation_file = "/data/reddylab/Reference_Data/IGVF/GRCh38/Gencode/v43/IGVFFI7217ZMJZ.gtf"
+            self.rsem_dir = "/data/reddylab/Reference_Data/IGVF/GRCh38/RSEM_genome%s"% (".with_ercc92" if with_ercc else "")
             self.genome_sizes_file = "/data/reddylab/Reference_Data/IGVF/GRCh38/GCA_000001405.15_GRCh38_no_alt_analysis_set%s.sizes" % (".with_ercc92" if with_ercc else "")
-            self.genome_fasta_files = ["/data/reddylab/Reference_Data/IGVF/GRCh38/fasta/IGVFFI0653VCGH.fasta" % (".with_ercc92" if with_ercc else "")]
+            self.genome_fasta_files = ["/data/reddylab/Reference_Data/IGVF/GRCh38/fasta/IGVFFI0653VCGH.fasta"]
             self.regions_bed_file = "/data/reddylab/Reference_Data/IGVF/GRCh38/GCA_000001405.15_GRCh38_no_alt_analysis_set%s.bed" % (".with_ercc92" if with_ercc else "")
         elif _genome == 'hg19' or _genome == 'grch37':
             self.bowtie_genome_ref_first_index = "/data/reddylab/Reference_Data/Genomes/hg19/hg19.1.ebwt"
@@ -70,13 +70,13 @@ class ReferenceDataset(object):
             self.bowtie_genome_ref_first_index = "/data/reddylab/Reference_Data/IGVF/GRCm39/bowtie_GRCm39/IGVFFI928QLXO_GRCm39.1.ebwt"
             self.bowtie2_genome_ref_first_index = "/data/reddylab/Reference_Data/IGVF/GRCm39/bowtie2_GRCm39/IGVFFI928QLXO_GRCm39.1.bt2"
             self.star_genome_dir = "/data/reddylab/Reference_Data/IGVF/GRCm39/STAR_GRCm39_genome"
-            self.genome_sizes_file = "/data/reddylab/Reference_Data/IGVF/GRCm39/Gencode/mm39.chrom.sizes"
-            self.encode_blacklist_bedfile = "/data/reddylab/Reference_Data/ENCODE/mm10.blacklist.bed" #TODO: Create liftover from mm10 to mm39
+            self.genome_sizes_file = "/data/reddylab/Reference_Data/IGVF/GRCm39/mm39.chrom.size"
+            self.encode_blacklist_bedfile = "/data/reddylab/Reference_Data/IGVF/GRCm39/exclusion_lists/mm39.excluderanges.bed"
             self.genome_effective_size = "mm"
             self.annotation_file = "/data/reddylab/Reference_Data/IGVF/GRCm39/Gencode/vM32/IGVFFI9744VSJF.gtf"
             self.rsem_dir = "/data/reddylab/Reference_Data/IGVF/GRCm39/RSEM_genome"
             self.genome_fasta_files = ["/data/reddylab/Reference_Data/IGVF/GRCm39/fasta/IGVFFI9282QLXO.fasta"]
-            self.regions_bed_file = "/data/reddylab/Reference_Data/Genomes/mm9/mm9.chrom.bed" #TODO: Update to mm39
+            self.regions_bed_file = "/data/reddylab/Reference_Data/IGVF/GRCm39/mm39.chrom.bed"
         elif _genome == 'danrer10' or _genome == 'grcz10':
             self.bowtie_genome_ref_first_index = "/data/reddylab/Reference_Data/Genomes/danRer10/bowtie/danRer10.1.ebwt"
             self.star_genome_dir = "/data/reddylab/Reference_Data/Genomes/danRer10/STAR_genome_sjdbOverhang_%d_novelSJDB" % self.read_length
